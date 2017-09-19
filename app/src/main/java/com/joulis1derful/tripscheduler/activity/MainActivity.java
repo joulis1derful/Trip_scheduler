85435170a2a7db774c2f9a4fc083a771ba1d514f
joulis1derful <joulis1derful@gmail.com>
2017-09-19 22:25:39 +0300

    Added progress bar, displaying list that was passed by asynctask

--- app/src/main/java/com/joulis1derful/tripscheduler/activity/MainActivity.java
+++ app/src/main/java/com/joulis1derful/tripscheduler/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.joulis1derful.tripscheduler.R;
@@ -15,53 +17,81 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView emptyList;
+    private RecyclerView mRecycler;
+    private ProgressBar mProgressBar;
+
     private TripInfoAdapter mAdapter;
     private List<TripInfo> trips;
 
-    private List<String> result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        parseData();
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        emptyList = (TextView) findViewById(R.id.list_is_empty);
+        mRecycler = (RecyclerView) findViewById(R.id.recycler_view);
+        showProgressBar(true);
 
-        refreshUI();
+        parseData();
+//        refreshUI();
 
     }
 
     private void refreshUI() {
-        trips = new ArrayList<>();
-        TripInfo.City city1 = new TripInfo.City(1, 2, "Kiev");
-        TripInfo.City city2 = new TripInfo.City(2, 1, "Chernigov");
-
-        TripInfo trip1 = new TripInfo(city1, city2, "15.08.2017", "16.08.2017", "15:00", "01:00",
-                "Da", "Net", "lol", 250, 15, 0);
-        TripInfo trip2 = new TripInfo(city2, city1, "15.08.2017", "16.08.2017", "15:00", "01:00",
-                "Da", "Net", "lol", 250, 15, 0);
-        trips.add(trip1);
-        trips.add(trip2);
-        TextView emptyList = (TextView) findViewById(R.id.list_is_empty);
-        RecyclerView mRecycler = (RecyclerView) findViewById(R.id.recycler_view);
+//        trips = new ArrayList<>();
+//        TripInfo.City city1 = new TripInfo.City(1, 2, "Kiev");
+//        TripInfo.City city2 = new TripInfo.City(2, 1, "Chernigov");
+//
+//        TripInfo trip1 = new TripInfo(1, city1, city2, "15.08.2017", "16.08.2017", "15:00", "01:00",
+//                "Da", "Net", "lol", 250, 15, 0);
+//        TripInfo trip2 = new TripInfo(2, city2, city1, "15.08.2017", "16.08.2017", "15:00", "01:00",
+//                "Da", "Net", "lol", 250, 15, 0);
+//        trips.add(trip1);
+//        trips.add(trip2)
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         mRecycler.setLayoutManager(layoutManager);
         mRecycler.setHasFixedSize(true);
         mAdapter = new TripInfoAdapter(this, trips);
         mRecycler.setAdapter(mAdapter);
-        mAdapter.notifyDataSetChanged();
+//        mAdapter.notifyDataSetChanged();
+        showProgressBar(false);
+
+    }
+
+    private void checkParseResults() {
+        if(trips.isEmpty()) {
+            mRecycler.setVisibility(View.GONE);
+            emptyList.setVisibility(View.VISIBLE);
+        } else {
+            emptyList.setVisibility(View.GONE);
+            mRecycler.setVisibility(View.VISIBLE);
+        }
     }
 
     private void parseData() {
-        result = new ArrayList<>();
-        new ParseTripInfo(new ParseTripInfo.ParseResponse() {
+        trips = new ArrayList<>();
+        ParseTripInfo parsingObj = new ParseTripInfo(new ParseTripInfo.ParseResponse() {
             @Override
-            public void processFinish(List<String> results) {
-                result.addAll(results);
-//                for(String s : result) {
-//                    Log.d("RESULTS", s);
-//                }
+            public void processFinish(List<TripInfo> results) {
+                //  showProgressBar(true);
+                trips.addAll(results);
+                refreshUI();
+                checkParseResults();
             }
-        }).execute();
+        });
+        parsingObj.execute();
+    }
+
+    private void showProgressBar(boolean state) {
+        if(state) {
+            mRecycler.setVisibility(View.INVISIBLE);
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+            mRecycler.setVisibility(View.VISIBLE);
+        }
     }
 
 }
