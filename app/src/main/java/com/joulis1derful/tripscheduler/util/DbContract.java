9d3eeb1365c42a219a31280d6bdff624af7d98b9
joulis1derful <joulis1derful@gmail.com>
2017-09-21 23:19:43 +0300

    Constants for db

---
+++ app/src/main/java/com/joulis1derful/tripscheduler/util/DbContract.java
@@ -0,0 +1,22 @@
+package com.joulis1derful.tripscheduler.util;
+
+public class DbContract {
+    public static final String TABLE_NAME = "trips";
+    public static final String KEY_ID = "_id";
+    public static final String KEY_CITY1_ID = "city1_id";
+    public static final String KEY_CITY1_HIGHLIGHT = "city1_highlight";
+    public static final String KEY_CITY1_NAME = "city1_name";
+    public static final String KEY_CITY2_ID = "city2_id";
+    public static final String KEY_CITY2_HIGHLIGHT = "city2_highlight";
+    public static final String KEY_CITY2_NAME = "city2_name";
+    public static final String KEY_DATE_FROM = "date_from";
+    public static final String KEY_DATE_TO = "date_to";
+    public static final String KEY_TIME_FROM = "time_from";
+    public static final String KEY_TIME_TO = "time_to";
+    public static final String KEY_INFO_FROM = "info_from";
+    public static final String KEY_INFO_TO = "info_to";
+    public static final String KEY_INFO = "info";
+    public static final String KEY_PRICE = "price";
+    public static final String KEY_BUS_ID = "bus_id";
+    public static final String KEY_RESERVATION_COUNT = "reservation_count";
+}
