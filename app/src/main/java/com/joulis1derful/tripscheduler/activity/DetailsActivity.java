735cadf0fb7a5aef5a8537a9cfc4b403f1047bfd
joulis1derful <joulis1derful@gmail.com>
2017-09-24 16:43:43 +0300

    Details page class

---
+++ app/src/main/java/com/joulis1derful/tripscheduler/activity/DetailsActivity.java
@@ -0,0 +1,15 @@
+package com.joulis1derful.tripscheduler.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.joulis1derful.tripscheduler.R;
+
+public class Details_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_);
+    }
+}
